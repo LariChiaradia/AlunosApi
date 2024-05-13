a2724456dc1499d2e93c8565aaae99a030eea4ba
Larissa Chiaradia Da Silva <larissa.silva@fcamara.com>
2024-05-13 17:40:21 -0300

    feat: Criada a Interface e Service de Aluno

---
+++ AlunosApi/Controllers/AlunosController.cs
@@ -0,0 +1,17 @@
+﻿using AlunosApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlunosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunosController : Controller
+    {
+        private readonly IAlunoService _service;
+
+        public AlunosController(IAlunoService service)
+        {
+            _service = service;
+        }
+    }
+}
